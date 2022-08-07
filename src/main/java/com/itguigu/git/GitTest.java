bd65dead99ab09c79cc9072e52dd5816d6791cd5
yansen <2112045409@qq.com>
2022-08-07 14:35:29 +0800

    second commit

--- src/main/java/com/itguigu/git/GitTest.java
+++ src/main/java/com/itguigu/git/GitTest.java
@@ -3,6 +3,6 @@ package com.itguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
-
 }
