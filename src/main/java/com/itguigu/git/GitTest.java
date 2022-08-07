aaf0bb72163b107d410120ce3e66647f0db0e0b5
杨 <110437971+yansenyc@users.noreply.github.com>
2022-08-07 20:29:16 +0800

    Update GitTest.java

--- src/main/java/com/itguigu/git/GitTest.java
+++ src/main/java/com/itguigu/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("master test");
         System.out.println("hot-fix test");
         System.out.println("测试");
+        System.out.println("我这里修改了");
     }
 }
